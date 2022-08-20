c473868261ae127cb603fd3271b4b0f29286f416
sinxenon4 <sinxenon4@gmail.com>
2022-08-20 20:05:16 +0300

    Add sticky nav bar; adjust alignment

---
+++ src/Header.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function Header() {
+    return (
+        <div class='heading-container'>
+            <div class='heading-container-text heading-container-main-text'>Currency converter</div>
+            <div class='heading-container-text'>Check live foreign currency exchange rates <br />
+                Inspired by Xe design. Using exchangeratesapi.io
+            </div>
+        </div>
+    )
+}
--- src/NavigationBar.js
+++ src/NavigationBar.js
@@ -4,13 +4,13 @@ export default function NavigationBar() {
     return (
 
         <nav class='nav-header'>
+            <img src='icons/dollar_white.png' alt="Logo" />
             <ul class="menuItems">
-                <li><img src='icons/dollar_white.png' alt="Logo" /></li>
-                <li><a href='#' data-item='Personal'>Personal</a></li>
-                <li><a href='#' data-item='Business'>Business</a></li>
-                <li><a href='#' data-item='Converter'>Converter</a></li>
-                <li><a href='#' data-item='Currency API'>Currency API</a></li>
-                <li><a href='#' data-item='Tools'>Tools</a></li>
+                <li><a href='/' data-item='Personal'>Personal</a></li>
+                <li><a href='/' data-item='Business'>Business</a></li>
+                <li><a href='/' data-item='Converter'>Converter</a></li>
+                <li><a href='/' data-item='Currency API'>Currency API</a></li>
+                <li><a href='/' data-item='Tools'>Tools</a></li>
             </ul>
         </nav>
 
