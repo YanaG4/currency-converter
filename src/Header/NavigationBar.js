7950c6660c7406018fb678c0d429b72c8f6e7763
sinxenon4 <sinxenon4@gmail.com>
2022-08-26 02:23:09 +0300

    Add focus on color-theme switch

--- src/Header/NavigationBar.js
+++ src/Header/NavigationBar.js
@@ -8,10 +8,10 @@ export default function NavigationBar() {
             <img src='icons/dollar_white.png' alt="Logo" />
             <div className='menu'><a href='/'>☰</a>
                 <ul className="menuItems dropdown">
-                    <li className='link'><a href='/' data-item='Converter'>Converter</a></li>
-                    <li className='link'><a href='/' data-item='Currency API'>Currency API</a></li>
-                    <li className='link'><a href='/' data-item='Tech stack'>Tech stack</a></li>
-                    <li className='link'><a href='/' data-item='About'>About</a></li>
+                    <li><a href='/' data-item='Converter'>Converter</a></li>
+                    <li><a href='/' data-item='Currency API'>Currency API</a></li>
+                    <li><a href='/' data-item='Tech stack'>Tech stack</a></li>
+                    <li><a href='/' data-item='About'>About</a></li>
                 </ul>
             </div>
             <ToggleTheme />
