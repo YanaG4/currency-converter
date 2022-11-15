71802df9faf7b572df57c9cf763d1463bd9acec2
sinxenon4 <sinxenon4@gmail.com>
2022-11-16 01:14:47 +0200

    Add screen adaptiveness for mobile section; Code refactoring

--- src/components/CurrencyConverter/CurrencyInfoSection/CurrencyInfoSection.js
+++ src/components/CurrencyConverter/CurrencyInfoSection/CurrencyInfoSection.js
@@ -1,8 +1,9 @@
 import React, { useEffect } from 'react'
-import './CurrencyInfoSection.css'
-
+//redux
 import { useDispatch, useSelector } from 'react-redux'
 import { getFacts, fetchInterestingFacts } from '../../../features/interestingFacts/interestingFactsSlice'
+//styles
+import './CurrencyInfoSection.css'
 
 export default function CurrencyInfoSection() {
     const dispatch = useDispatch()
@@ -20,15 +21,13 @@ export default function CurrencyInfoSection() {
     return (
         <div className='data-container text-container'>
             <div className='text-header-background'></div>
-            <div className='text'>
-                <h3>Did you know?..</h3>
-                <br />
-                <h4>{currencyHistoryText.title}</h4>
-                <br />
-                <p dangerouslySetInnerHTML={{ __html: textWithNewLine() }} />
-                <br />
-                <a href={currencyHistoryText.source} target="_blank" rel="noopener">Source link</a>
-            </div>
+            <h3>Did you know?..</h3>
+            <br />
+            <h4>{currencyHistoryText.title}</h4>
+            <br />
+            <p dangerouslySetInnerHTML={{ __html: textWithNewLine() }} />
+            <br />
+            <a href={currencyHistoryText.source} target="_blank" rel="noopener">Source link</a>
         </div>
     )
 }
--- src/components/MobileSection/MobileSection.js
+++ src/components/MobileSection/MobileSection.js
@@ -1,29 +1,26 @@
 import React from 'react'
-import './MobileSection.scss'
 import ToggleTheme from '../Elements/ToggleTheme/ToggleTheme'
+import './MobileSection.scss'
 
 export default function MobileSection() {
     return (
-        <>
-            <div className='mobile-container'>
-                <img src='images/iphone_ipad2.png' alt='Mobile and tablet view of the currency converter.' />
-                <div className='mobile-text'>
-                    <h4>Responsive Design.</h4>
-                    <div className='text'>
-                        Check live rates, send money securely, set rate alerts, receive notifications and more</div>
-                    <div className='text small-text'>
-                        Using Pure CSS.</div>
-
-                    <h4>Try color change!</h4>
-                    <div className='text'>
-                        Pick a color scheme that suits you.<br /> Warm for a more comforting experience and cold for a more focused one.</div>
-                    <div className='button'>
-                        <ToggleTheme />
-                    </div>
-                    <div className='text small-text'>
-                        Using SCSS.</div>
+        <div className='mobile-container'>
+            <img src='images/iphone_ipad2.png' alt='Mobile and tablet view of the currency converter.' />
+            <div className='mobile-text'>
+                <h4>Responsive Design.</h4>
+                <div>
+                    Check live rates, send money securely, set rate alerts, receive notifications and more</div>
+                <div className='small-text'>Using Pure CSS.</div>
+                <h4>Try color change!</h4>
+                <div>
+                    Pick a color scheme that suits you.
+                    <br />
+                    Warm for a more comforting experience and cold for a more focused one.</div>
+                <div className='button'>
+                    <ToggleTheme />
                 </div>
+                <div className='small-text'>Using SCSS.</div>
             </div>
-        </>
+        </div>
     )
 }
