427158607dd2e15ae1689e2f5e7fb972e5cb4d08
sinxenon4 <sinxenon4@gmail.com>
2022-11-15 16:13:55 +0200

    Prevent data from caching

--- src/api/currencyApi.js
+++ src/api/currencyApi.js
@@ -2,4 +2,8 @@ import axios from "axios";
 
 export default axios.create({
     baseURL: "https://api.exchangerate.host/",
+    //prevent data from caching
+    params: {
+        timestamp: new Date().getTime()
+    }
 })
