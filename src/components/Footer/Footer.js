87da417dc1a998500cc88529d8e995c5fe74120a
YanaG4 <sinxenon4@gmail.com>
2024-04-15 17:31:43 +0300

    Change links to github

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -4,8 +4,8 @@ import './Footer.css'
 export default function Footer() {
     return (
         <footer>
-            <div>Made by <a href='https://github.com/sinxenon' className='footer-author' target='_blank' rel="noreferrer">SinXenon</a></div>
-            <div className='footer-date'>© 2022 - 2023</div>
+            <div>Made by <a href='https://github.com/yanag4' className='footer-author' target='_blank' rel="noreferrer">Yana</a></div>
+            <div className='footer-date'>© 2022 - 2024</div>
             <div className='footer-fullwidth'></div>
         </footer>
     )
